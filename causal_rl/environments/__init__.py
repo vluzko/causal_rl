30cd5d5e331b0d16eb5c4d6bef8d70e09fa0a935
vluzko <vincent.luczkow@gmail.com>
2020-12-15 10:53:28 -0500

    Renamed hard spheres file

--- causal_rl/environments/__init__.py
+++ causal_rl/environments/__init__.py
@@ -1,6 +1,6 @@
 # from causal_learning import SupervisedCDAG
 
 from .causal_env import CausalEnv
-from .bouncing_balls import HardSpheres
+from .hard_spheres import HardSpheres
 from .multi_typed import MultiTyped, WithTypes
 from .mujoco import Mujoco
