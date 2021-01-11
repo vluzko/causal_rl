5be24985b293edb540f2b919de79811addd243b7
vluzko <vincent.luczkow@gmail.com>
2021-01-11 02:01:57 -0500

    Travis test

--- test/env_test.py
+++ test/env_test.py
@@ -1,9 +1,9 @@
 import causal_rl
-# from causal_rl.environments import HardSpheres
+from causal_rl.environments import HardSpheres
 
 
 def test_hard_spheres():
-    dir(causal_rl)
+    print(dir(causal_rl))
     # num_obj = 2
     # steps = 100
     # env = causal_rl.environments.HardSpheres(num_obj=num_obj)
