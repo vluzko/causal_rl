f2259b6316df8841b0aeb67ff6a30e02ac00fdf3
vluzko <vincent.luczkow@gmail.com>
2021-01-11 01:40:27 -0500

    Travis test

---
+++ causal_rl/__init__.py
@@ -0,0 +1,1 @@
+import causal_rl.environments
--- causal_rl/environments/__init__.py
+++ causal_rl/environments/__init__.py
@@ -1,5 +1,3 @@
-# from causal_learning import SupervisedCDAG
-
 from .causal_env import CausalEnv
 from .hard_spheres import HardSpheres
 from .multi_typed import MultiTyped, WithTypes
